45838d7e86754ac0935a3756ed58407e1d694eb4
RichardBrowning <mi888uzl@outlook.com>
2023-01-20 07:37:57 +0800

    update Linked List node

---
+++ doubleTypeArray/LinkedListNode.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+
+LinkedListedNode::LinkedListedNode()
+{
+    data = 0;
+    next = NULL;
+    prev = NULL;
+}
+
+LinkedListedNode::~LinkedListedNode()
+{
+    delete next;
+    delete prev;
+}
+
+int LinkedListedNode::getData()
+{
+    return data;
+}
+
+LinkedListedNode* LinkedListedNode::getNext()
+{
+    return next;
+}
+
+void LinkedListedNode::setData(int data)
+{
+    this->data = data;
+}
+
+void LinkedListedNode::setNext(LinkedListedNode* next)
+{
+    this->next = next;
+}
+
+LinkedListedNode* LinkedListedNode::getPrev()
+{
+    return prev;
+}
+
+void LinkedListedNode::setPrev(LinkedListedNode* prev)
+{
+    this->prev = prev;
+}
---
+++ doubleTypeArray/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include "rand-normal.cpp"
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << rand_normal(0, 1) << std::endl;
+    }
+    return 0;
+}
