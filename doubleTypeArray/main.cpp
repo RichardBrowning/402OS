dd076e664dde3582ef4cf510c41d5a1dd7cd587d
RichardBrowning <mi888uzl@outlook.com>
2023-02-08 01:33:09 +0800

    add os checker

--- doubleTypeArray/main.cpp
+++ doubleTypeArray/main.cpp
@@ -2,12 +2,14 @@
 #include <math.h>
 #include <stdlib.h>
 #include "rand-normal.h"
+#include "OSCheck.h"
 
 int main()
 {
-    for (int i = 0; i < 10; i++)
-    {
-        std::cout << rand_normal(0, 1) << std::endl;
-    }
+    std::cout << getOsName() << std::endl;
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     std::cout << rand_normal(0, 1) << std::endl;
+    // }
     return 0;
 }
