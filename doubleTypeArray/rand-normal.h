b3b5490093ea1569fccb15771cd5529e8a559c4a
RichardBrowning <mi888uzl@outlook.com>
2023-02-08 02:15:28 +0800

    use rand(), awaiting instruction

--- doubleTypeArray/rand-normal.h
+++ doubleTypeArray/rand-normal.h
@@ -2,9 +2,8 @@
 #define RAND_NORMAL
 
 /** get a random number between 0 and 1 */
-#include <random>
 #include <math.h>
-#define UNIT_RAND() (random() / (1.0+RAND_MAX))
+#define UNIT_RAND() (rand() / (1.0+RAND_MAX))
 
 double rand_normal(double mean, double dev);
 
