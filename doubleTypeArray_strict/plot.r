0037ce43c9c92ab2952c322896f4ea6325576512
RichardBrowning <46521212+RichardBrowning@users.noreply.github.com>
2023-02-10 12:02:36 +0800

    updates stats

--- doubleTypeArray_strict/plot.r
+++ doubleTypeArray_strict/plot.r
@@ -1,9 +1,18 @@
-consensus.data <- read.csv(file = "consensus-size.csv",  header = TRUE, sep = ",")
-#get dot plot
-plot(consensus.data$consensus.time ~ consensus.data$size)
-#linear regression
-fit<-lm(consensus.data$consensus.time ~ poly(consensus.data$size, 3, raw=TRUE), data = consensus.data)
-#draw curve
-#curve(coef(mod.log)[1] + coef(mod.log)[2]*(x^2), add=TRUE)
-lines(consensus.data$size, predict(fit, data.frame(x=consensus.data$size)), col='red')
+consensus.size <- read.csv(file = "consensus-size.csv",  header = TRUE, sep = ",")
+consensus.magnitude <- read.csv(file = "consensus-magnitude.csv",  header = TRUE, sep = ",")
+consensus.variance<- read.csv(file = "consensus-variance.csv",  header = TRUE, sep = ",")
 
+#linear regression
+fitSize<-lm(consensus.size$consensus.time ~ poly(consensus.size$size, 5, raw=TRUE), data = consensus.size)
+#get dot plot
+plot(consensus.size$consensus.time ~ consensus.size$size)
+#draw curve
+lines(consensus.size$size, predict(fitSize, data.frame(x=consensus.size$size)), col='red')
+
+fitMag <- lm(consensus.magnitude$consensus.time ~ consensus.magnitude$maginitude)
+plot(consensus.magnitude$consensus.time ~ consensus.magnitude$maginitude, asp=0.005)
+abline(fitMag, col = 'red')
+
+fitVar <- lm( consensus.variance$consensus.time ~ consensus.variance$variance )
+plot(consensus.variance$consensus.time ~ consensus.variance$variance)
+abline(fitVar, col = 'red')
